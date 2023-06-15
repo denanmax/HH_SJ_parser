78e3d124008a2ceacc4d94fe78c9754cd362aa0f
denanmax <deshis93@gmail.com>
2023-06-15 18:54:23 +0200

    json saver added

---
+++ json_saver.py
@@ -0,0 +1,18 @@
+import json
+
+
+class SaveToJson:
+    """Универсальный класс для сохранения информации о вакансиях в файл по определённым критериям"""
+
+    def __init__(self, keyword):
+        self.__filename = f"{keyword.title()}.json"
+
+    def filename(self):
+        return self.__filename
+
+    def add_vacancies(self, data):
+        """Метод для записи информации о подходящих вакансиях в json файл"""
+        with open(self.__filename, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
+
