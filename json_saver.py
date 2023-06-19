f236c5e153d53616e21aeee4eefbc90325b111f8
denanmax <deshis93@gmail.com>
2023-06-19 10:17:37 +0200

    small updates

--- json_saver.py
+++ json_saver.py
@@ -2,15 +2,15 @@ import json
 
 
 class SaveToJson:
-    """Универсальный класс для сохранения информации о вакансиях в файл по определённым критериям"""
+    """Класс для сохранения информации о вакансиях в файл"""
 
     def __init__(self, keyword):
-        self.__filename = f"{keyword.title()}.json"
+        self.__filename = f"{keyword}.json"
 
     def filename(self):
         return self.__filename
 
-    def add_vacancies(self, data):
+    def save_vacancies(self, data):
         """Метод для записи информации о подходящих вакансиях в json файл"""
         with open(self.__filename, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=4, ensure_ascii=False)
