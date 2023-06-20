b78153b972c4d02da6f72c8d07c985abb18d2ffc
denanmax <deshis93@gmail.com>
2023-06-20 11:54:12 +0200

    added information about filtered parametrs for vacancies

--- hh.py
+++ hh.py
@@ -11,8 +11,8 @@ class HeadHunterAPI(AbstractVacancyAPI):
     def request_api(self, keyword):
         """Метод запроса по API"""
         params = {
-            "text": keyword.title(),
-            "per_page": 100,
+            "text": keyword,
+            "per_page": 10,
         }
         headers = {"HH-User-Agent": 'VacancyMachine/1.0 (deshis93@gmail.com)'}
 
@@ -33,14 +33,15 @@ class HeadHunterAPI(AbstractVacancyAPI):
 
 
 class JSONSaverHH(SaveToJson):
-    """Класс для сохранения информации о вакансиях в файл по определённым критериям"""
+    """Класс для сохранения информации о вакансиях в файл"""
 
-    def json_read_hh(self):
+    def json_read(self):
         """Метод для чтения информации о вакансиях из json файла"""
-        with open(self.filename(), 'r', encoding='utf-8') as file:
+        with open(self.filename(suffix='hh'), 'r', encoding='utf-8') as file:
             data = json.load(file)
 
         vacancies = []
+        filtered_data = []
         for row in data:
             salary_min = None
             salary_max = None
@@ -56,17 +57,36 @@ class JSONSaverHH(SaveToJson):
                                      row['employer']['name'],
                                      row['area']['name'],
                                      row['alternate_url']))
+
+        for vacancy in vacancies:
+            filtered_data.append({'Компания': vacancy.employer,
+                                  'Профессия': vacancy.title,
+                                  'От': vacancy.salary_from,
+                                  'До': vacancy.salary_to,
+                                  'Валюта': vacancy.currency,
+                                  'Город': vacancy.city,
+                                  'Ссылка': vacancy.link})
+
+        with open(f"HH_filtered.json", 'w', encoding='utf-8') as file:
+            json.dump(filtered_data, file, ensure_ascii=False, indent=4)
+
         return vacancies
 
+    def save_vacancies(self, data, suffix='hh'):
+        """Метод для записи информации о вакансиях в json файл"""
+        with open(self.filename(suffix), 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
 
-hh_keyword = 'морской'
+
+hh_keyword = 'химик'
 hh_api = HeadHunterAPI()
 
 hh_vacancies = hh_api.get_vacancies(hh_keyword)
 
 json_sever_hh = JSONSaverHH(hh_keyword)
 json_sever_hh.save_vacancies(hh_vacancies)
-data_hh = json_sever_hh.json_read_hh()
+data_hh = json_sever_hh.json_read()
 
 for i in data_hh:
     print(i)
+
