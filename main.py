27a5adcbf9990d4e73c85b77ed1c5e92ad1c2b5a
denanmax <deshis93@gmail.com>
2023-06-21 20:01:44 +0200

    added sort by salary min

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from utils.utils import hh_save_to_json, params_to_search, sj_save_to_json
+from utils.utils import hh_save_to_json, params_to_search, sj_save_to_json, sort_by_salary_max
+
 
 def main():
     print("Парсер вакансий для Head Hunter и Superjob")
     print("Внимание! Для работы с HH.ru необходимо указать хотя-бы один параметр из трех!")
 
-    check_input = input("Если хотите продолжить, нажмите д/н ")
+    check_input = input("Если хотите продолжить c HH.ru, нажмите д/н ")
     if check_input == "д".lower() or check_input == 'y'.lower():
         keyword, city, num_vacancies = params_to_search()
         hh_info = hh_save_to_json(keyword, city, num_vacancies)
+        hh_info = sort_by_salary_max(hh_info)
         try:
             for i in hh_info:
                 print(i)
@@ -18,6 +20,7 @@ def main():
     if user_input == "д".lower() or user_input == 'y'.lower():
         keyword, city, num_vacancies = params_to_search()
         info_sj = sj_save_to_json(keyword, city, num_vacancies)
+        info_sj = sort_by_salary_max(info_sj)
         for row in info_sj:
             print(row)
     else:
