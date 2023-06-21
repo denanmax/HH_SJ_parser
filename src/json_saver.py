b86a289e95e15890e364542a4f72cc9fe522c49c
denanmax <deshis93@gmail.com>
2023-06-21 17:26:05 +0200

    save vacancies transfered to json_saver.py

--- src/json_saver.py
+++ src/json_saver.py
@@ -1,3 +1,7 @@
+import json
+from src.Vacancy import Vacancy
+
+
 class SaveToJson:
     """Класс для сохранения информации о вакансиях в файл"""
 
@@ -7,11 +11,36 @@ class SaveToJson:
     def filename(self, suffix):
         return f"{self.keyword}_{suffix}.json"
 
-    @staticmethod
-    def save_vacancies(self, data, suffix=''):
+    def save_vacancies(self, data, suffix):
         """Метод для записи информации о вакансиях в json файл"""
-        pass
+        try:
+            vacancies = self.vacancy_list_to_print(data)
+        except:
+            pass
+
+        filtered_data = []
+
+        try:
+            for vacancy in vacancies:
+                filtered_data.append({'Компания': vacancy.employer,
+                                      'Профессия': vacancy.title,
+                                      'От': vacancy.salary_from,
+                                      'До': vacancy.salary_to,
+                                      'Валюта': vacancy.currency,
+                                      'Город': vacancy.city,
+                                      'Ссылка': vacancy.link})
+
+            with open(f"FILTERED_{self.filename(suffix).upper()}", 'w',
+                      encoding='utf-8') as file:  # тут сохраняем отфильтрованный json
+                json.dump(filtered_data, file, ensure_ascii=False, indent=4)
+
+            return vacancies
+        except UnboundLocalError:
+            pass
+
+        except TypeError:
+            pass
 
     @staticmethod
-    def json_filtered_vacancies(self):
+    def vacancy_list_to_print(self):
         pass
